e3dfe5b8ccc79b4092d12e328fb1e1671e9b7800
Murod Khaydarov <murod.haydarov@gmail.com>
2023-12-06 14:43:24 +0500

    HW02 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,65 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+// MyStringBuilder is a custom string Builder to extend its functionality
+type MyStringBuilder struct {
+	strings.Builder
+}
+
+// DeleteLast removes last byte from string
+func (b *MyStringBuilder) DeleteLast() {
+	s := b.String()
+	s = s[:len(s)-1]
+	b.Reset()
+	b.WriteString(s)
+}
+
+// RepeatLast repeats last byte `count` times
+func (b *MyStringBuilder) RepeatLast(count int) {
+	s := b.String()
+	repeatedLastChar := strings.Repeat(string(s[len(s)-1]), count)
+	b.WriteString(repeatedLastChar)
+}
+
+func Unpack(s string) (string, error) {
+	if s == "" {
+		return s, nil
+	}
+
+	if isDigit(s[0]) {
+		return "", ErrInvalidString
+	}
+
+	result := MyStringBuilder{}
+	for i := 0; i < len(s); i++ {
+		if isDigit(s[i]) {
+			if isDigit(s[i-1]) {
+				return "", ErrInvalidString
+			} else {
+				digit := convertDigit(s[i])
+				if digit == 0 {
+					result.DeleteLast()
+				} else {
+					result.RepeatLast(digit - 1)
+				}
+			}
+		} else {
+			result.WriteByte(s[i])
+		}
+	}
+
+	return result.String(), nil
+}
+
+func isDigit(b byte) bool {
+	return b >= '0' && b <= '9'
+}
+
+func convertDigit(b byte) int {
+	return int(b - '0')
 }
