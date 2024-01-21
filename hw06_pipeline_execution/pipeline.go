386c748eb89e40bbb48f2cf2d6dc0a11d133e2cb
Murod Khaydarov <murod.haydarov@gmail.com>
2024-01-21 20:06:53 +0500

    review improvements

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,24 +9,38 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	out := make(Bi)
-	go func() {
-		defer close(out)
-		for _, stage := range stages {
-			in = stage(in)
-		}
+	pipelineOut := make(Bi)
+	lastStageStream := in
+	for _, stage := range stages {
+		nextStageStream := make(Bi)
+		go func(stage Stage, in In, nextStageStream Bi) {
+			defer close(nextStageStream)
 
+			for v := range stage(in) {
+				select {
+				case nextStageStream <- v:
+				case <-done:
+					return
+				}
+			}
+		}(stage, lastStageStream, nextStageStream)
+		lastStageStream = nextStageStream
+	}
+
+	go func() {
+		defer close(pipelineOut)
 		for {
 			select {
 			case <-done:
 				return
-			case v, ok := <-in:
+			case v, ok := <-lastStageStream:
 				if !ok {
 					return
 				}
-				out <- v
+				pipelineOut <- v
 			}
 		}
 	}()
-	return out
+
+	return pipelineOut
 }
