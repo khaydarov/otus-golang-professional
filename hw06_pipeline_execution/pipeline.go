081106def60c0f416b6c7130adfc6831591f9935
Murod Khaydarov <murod.haydarov@gmail.com>
2024-01-23 17:41:57 +0500

    improve code

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -12,18 +12,32 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	pipelineOut := make(Bi)
 	lastStageStream := in
 	for _, stage := range stages {
-		nextStageStream := make(Bi)
-		go func(stage Stage, in In, nextStageStream Bi) {
-			defer close(nextStageStream)
+		// Creating intermediateStream to translate from global "in" channel to intermediary
+		intermediary := make(Bi)
+		go func(intermediary Bi, in In) {
+			defer close(intermediary)
 
-			for v := range stage(in) {
+			for v := range in {
+				select {
+				case intermediary <- v:
+				case <-done:
+					return
+				}
+			}
+		}(intermediary, lastStageStream)
+
+		// Creating nextStageStream to translate current iteration stage channel data to the next one
+		nextStageStream := make(Bi)
+		go func(stage Stage, intermediary In, nextStageStream Bi) {
+			defer close(nextStageStream)
+			for v := range stage(intermediary) {
 				select {
 				case nextStageStream <- v:
 				case <-done:
 					return
 				}
 			}
-		}(stage, lastStageStream, nextStageStream)
+		}(stage, intermediary, nextStageStream)
 		lastStageStream = nextStageStream
 	}
 
