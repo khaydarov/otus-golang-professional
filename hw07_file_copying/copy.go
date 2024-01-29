e50bf68f7ec011b3ca53cbec175a80cac299df8e
Murod Khaydarov <murod.haydarov@gmail.com>
2024-01-30 00:00:55 +0500

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +14,54 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fromFile, err := os.Open(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer fromFile.Close()
+
+	fromFileInfo, err := fromFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	if fromFileInfo.Size() < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer toFile.Close()
+
+	if offset != 0 {
+		fromFile.Seek(offset, io.SeekStart)
+	}
+
+	var bar *pb.ProgressBar
+	if limit != 0 {
+		barLimit := limit
+		if limit > fromFileInfo.Size() {
+			barLimit = fromFileInfo.Size()
+		}
+
+		if fromFileInfo.Size()-offset < limit {
+			barLimit = fromFileInfo.Size() - offset
+		}
+
+		bar = pb.Full.Start64(barLimit)
+		barReader := bar.NewProxyReader(fromFile)
+		io.CopyN(toFile, barReader, limit)
+		bar.Finish()
+
+		return nil
+	}
+
+	bar = pb.Full.Start64(fromFileInfo.Size() - offset)
+	barReader := bar.NewProxyReader(fromFile)
+	io.Copy(toFile, barReader)
+	bar.Finish()
+
 	return nil
 }
