b9ec2993243856217d79d16e21e875ff5d1f6572
Murod Khaydarov <murod.haydarov@gmail.com>
2024-02-10 01:08:39 +0500

    hw8 is completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,42 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	envFiles := []struct {
+		name    string
+		content string
+	}{
+		{
+			name:    "APP_PORT",
+			content: "8080",
+		},
+		{
+			name:    "APP_HOST",
+			content: "localhost",
+		},
+		{
+			name:    "APP_DEBUG",
+			content: "true",
+		},
+	}
+
+	_ = os.Mkdir("./testdata/temp/", os.ModePerm)
+	for _, envFile := range envFiles {
+		file, _ := os.Create("./testdata/temp/" + envFile.name)
+		file.WriteString(envFile.content)
+	}
+
+	envs, err := ReadDir("./testdata/temp/")
+	require.NoError(t, err)
+	require.Equal(t, envs["APP_PORT"].Value, "8080")
+	require.Equal(t, envs["APP_HOST"].Value, "localhost")
+	require.Equal(t, envs["APP_DEBUG"].Value, "true")
+
+	os.RemoveAll("./testdata/temp/")
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,12 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	returnCode := RunCmd([]string{"echo", "hello"}, Environment{})
+	require.Equal(t, 0, returnCode)
 }
