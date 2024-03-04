91b435a8b9627da6305480afb7f4640be3198893
Murod Khaydarov <murod.haydarov@gmail.com>
2024-03-04 15:00:19 +0500

    initial

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,57 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
+var duration time.Duration
+
+func init() {
+	flag.DurationVar(&duration, "timeout", 0, "timeout")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 2 {
+		fmt.Println("host and port are required")
+		return
+	}
+
+	host := args[0]
+	port := args[1]
+
+	address := net.JoinHostPort(host, port)
+
+	in := bufio.NewReader(os.Stdin)
+
+	client := NewTelnetClient(address, duration, in, os.Stdout)
+	err := client.Connect()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	os.Stderr.WriteString(fmt.Sprintf("...Connected to %s\n", address))
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		client.Send()
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		client.Receive()
+	}()
+
+	wg.Wait()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -13,9 +16,77 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &GoTelnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+type GoTelnetClient struct {
+	conn    net.Conn
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+}
+
+func (c *GoTelnetClient) Connect() error {
+	var err error
+	dialer := &net.Dialer{}
+	dialer.Timeout = c.timeout
+	c.conn, err = dialer.Dial("tcp", c.address)
+	if err != nil {
+		return err
+	}
+
+	clientMessagesChannel := ioReaderChannel(c.in)
+
+	defer c.conn.Close()
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *GoTelnetClient) Close() error {
+	err := c.conn.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *GoTelnetClient) Send() error {
+	var err error
+	for message := range clientMessagesChannel {
+		_, err = c.conn.Write([]byte(fmt.Sprintf("%s\n", message)))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *GoTelnetClient) Receive() error {
+	var err error
+	buf := bufio.NewScanner(c.conn)
+	for buf.Scan() {
+		_, err = c.out.Write([]byte(fmt.Sprintf("%s\n", buf.Text())))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func ioReaderChannel(in io.ReadCloser) <-chan string {
+	out := make(chan string)
+	go func() {
+		defer close(out)
+		buf := bufio.NewScanner(in)
+		for buf.Scan() {
+			out <- buf.Text()
+		}
+	}()
+	return out
+}
