a9ef439cc0124eedd069378d5bdca096c3eebcfd
Murod Khaydarov <murod.haydarov@gmail.com>
2025-04-02 23:24:21 +0500

    optimised pipeline

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,53 +8,31 @@ type (
 
 type Stage func(in In) (out Out)
 
-func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	pipelineOut := make(Bi)
-	lastStageStream := in
-	for _, stage := range stages {
-		// Creating intermediateStream to translate from global "in" channel to intermediary
-		intermediary := make(Bi)
-		go func(intermediary Bi, in In) {
-			defer close(intermediary)
-
-			for v := range in {
-				select {
-				case intermediary <- v:
-				case <-done:
-					return
-				}
-			}
-		}(intermediary, lastStageStream)
-
-		// Creating nextStageStream to translate current iteration stage channel data to the next one
-		nextStageStream := make(Bi)
-		go func(stage Stage, intermediary In, nextStageStream Bi) {
-			defer close(nextStageStream)
-			for v := range stage(intermediary) {
-				select {
-				case nextStageStream <- v:
-				case <-done:
-					return
-				}
-			}
-		}(stage, intermediary, nextStageStream)
-		lastStageStream = nextStageStream
-	}
-
+func stageWithDone(in In, done In, stage Stage) Out {
+	out := make(Bi)
 	go func() {
-		defer close(pipelineOut)
+		defer close(out)
+		stageStream := stage(in)
 		for {
 			select {
 			case <-done:
 				return
-			case v, ok := <-lastStageStream:
+			case v, ok := <-stageStream:
 				if !ok {
 					return
 				}
-				pipelineOut <- v
+				out <- v
 			}
 		}
 	}()
+	return out
+}
 
-	return pipelineOut
+func ExecutePipeline(in In, done In, stages ...Stage) Out {
+	result := in
+	for _, stage := range stages {
+		result = stageWithDone(result, done, stage)
+	}
+
+	return result
 }
